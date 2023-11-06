66c1f4c04a34c3bb8898b1d596eb3c9c6069f46b
mandy0207 <mandyatwork24@gmail.com>
2023-11-05 21:53:40 -0500

    Multiple Browsers

--- src/MultipleBrowsers.java
+++ src/MultipleBrowsers.java
@@ -1,9 +1,32 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class MultipleBrowsers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String browserName = "Chrome";
+		WebDriver driver= null;
+		
+		if (browserName.equals("Chrome")) {
+			 driver = new ChromeDriver();
+			
+		} else if (browserName.equals("Firefox")) {
+			 driver = new FirefoxDriver();
+		}
+
+		else if (browserName.equals("Edge")) {
+		 driver = new EdgeDriver();
+		}
+		else {
+			System.out.println("Choose correct browser");
+		}
+		
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.in/");
+        driver.quit();
 	}
 
 }
