cd5f7671afe3c05d4d0cdc072f7e3b565460d684
mandy0207 <mandyatwork24@gmail.com>
2023-11-06 22:05:13 -0500

    Locators

--- src/SeleniumLocators.java
+++ src/SeleniumLocators.java
@@ -1,10 +1,11 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumLocators {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
@@ -17,7 +18,20 @@ public class SeleniumLocators {
 		//using class locator
 		driver.findElement(By.className("submit-button")).click();
 		
+     	driver.findElement(By.id("react-burger-menu-btn")).click();
+		
+		Thread.sleep(2000);
+		//using LinkText locator
+		driver.findElement(By.linkText("About")).click();
+		
+		driver.navigate().back();
+		
 		driver.findElement(By.id("react-burger-menu-btn")).click();
+		Thread.sleep(2000);
+		//using PartialLinkText locator
+		driver.findElement(By.partialLinkText("Logou")).click();
+		
+		driver.quit();
 		
 
 	}
