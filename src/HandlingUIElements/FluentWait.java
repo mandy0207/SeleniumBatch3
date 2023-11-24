138846c12e9485735a041a43943ffd3d374a6666
mandy0207 <mandyatwork24@gmail.com>
2023-11-23 22:38:29 -0500

    Fluent Waits

--- src/HandlingUIElements/FluentWait.java
+++ src/HandlingUIElements/FluentWait.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 
-public class WaitsExample {
+public class FluentWait {
 
 	public static void main(String[] args) {
 
